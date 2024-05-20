f5f18de73bc7a59a02f64489909aae49eb2ac7fb
José Alfredo Sorcia Cuellar <1721110137@utectulancingo.edu.mx>
2024-05-20 22:38:43 +0000

    funca

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 import json
-import request
+import requests
+
+url = "http://localhost:11434/api/generate"
+headers = {"Content-Type": "application/json"}
+data = {
+    "model": "llama2",
+    "prompt": "¿Por qué el cielo es azul?",
+    "system": "Responde como si fueras Son Goku",
+    "stream": False
+}
+
+response = requests.post(url, headers=headers, data=json.dumps(data))
+
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(f"Error: {response.status_code}")
